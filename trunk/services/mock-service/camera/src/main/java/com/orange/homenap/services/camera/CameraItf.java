60f5d6d62e5cf4b3ff456b703828c49d1e9da6a1
rdruilhe <remi.druilhe@orange.com>
2013-02-15 14:25:40 +0100

    Refactor of the mock applications

---
+++ trunk/services/mock-service/camera/src/main/java/com/orange/homenap/services/camera/CameraItf.java
@@ -0,0 +1,30 @@
+/*
+ * --------------------------------------------------------
+ * Module Name : component1
+ * Version : 0.1-SNAPSHOT
+ *
+ * Software Name : HomeNap
+ * Version : 0.1-SNAPSHOT
+ *
+ * Copyright © 28/06/2012 – 31/12/2013 France Télécom
+ * This software is distributed under the Apache 2.0 license,
+ * the text of which is available at http://www.apache.org/licenses/LICENSE-2.0.html
+ * or see the "LICENSE-2.0.txt" file for more details.
+ *
+ * --------------------------------------------------------
+ * File Name   : Component1Itf.java
+ *
+ * Created     : 20/11/2012
+ * Author(s)   : Remi Druilhe
+ *
+ * Description :
+ *
+ * --------------------------------------------------------
+ */
+
+package com.orange.homenap.services.camera;
+
+public interface CameraItf
+{
+    public String getText1();
+}
